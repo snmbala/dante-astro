b32543315369ec9885da8dc2348666f915979867
balamm <snmbala@gmail.com>
2025-02-09 23:16:58 +0530

    Updated the config

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,21 +1,21 @@
-import { defineStackbitConfig } from '@stackbit/types';
-import { GitContentSource } from '@stackbit/cms-git';
-import { allModels } from './.stackbit/models';
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+import { allModels } from "./.stackbit/models"; // Ensure the correct path
 
-const config = defineStackbitConfig({
-    stackbitVersion: '~0.7.0',
-    ssgName: 'astro',
-    nodeVersion: '18',
+export default defineStackbitConfig({
+    stackbitVersion: "~0.7.0",
+    ssgName: "astro",
+    nodeVersion: "18",
     contentSources: [
         new GitContentSource({
             rootPath: __dirname,
-            contentDirs: ['src/content'],
+            contentDirs: ["src/content"],
             models: allModels,
             assetsConfig: {
-                referenceType: 'static',
-                staticDir: 'src/content',
-                uploadDir: '_images',
-                publicPath: '/src/content'
+                referenceType: "static",
+                staticDir: "src/content",
+                uploadDir: "_images",
+                publicPath: "/src/content"
             }
         })
     ],
