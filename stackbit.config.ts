6c92338af298e780f88e1a75ae3df0441884492e
balamm <snmbala@gmail.com>
2025-02-09 23:18:31 +0530

    removed the content

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -6,17 +6,4 @@ export default defineStackbitConfig({
     stackbitVersion: "~0.7.0",
     ssgName: "astro",
     nodeVersion: "18",
-    contentSources: [
-        new GitContentSource({
-            rootPath: __dirname,
-            contentDirs: ["src/content"],
-            models: allModels,
-            assetsConfig: {
-                referenceType: "static",
-                staticDir: "src/content",
-                uploadDir: "_images",
-                publicPath: "/src/content"
-            }
-        })
-    ],
 });
