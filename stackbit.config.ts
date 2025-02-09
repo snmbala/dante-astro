2e929b32440f44571d6a740280fe8d4fd95e490d
balamm <snmbala@gmail.com>
2025-02-09 22:10:10 +0530

    Return to old config

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,30 +1,10 @@
-import { defineStackbitConfig } from "@stackbit/types";
-import { GitContentSource } from "@stackbit/cms-git";
+import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-  stackbitVersion: "~0.6.0",
-  nodeVersion: "18",
-  ssgName: "astro",
-  postInstallCommand: "npm i --no-save @stackbit/types",
-  contentSources: [
-    new GitContentSource({
-      rootPath: __dirname,
-      contentDirs: ["content"],
-      models: [
-        {
-          name: "Page",
-          type: "page",
-          urlPath: "/{slug}",
-          filePath: "content/pages/{slug}.md",
-          fields: [{ name: "title", type: "string", required: true }]
-        }
-      ],
-      assetsConfig: {
-        referenceType: "static",
-        staticDir: "public",
-        uploadDir: "images",
-        publicPath: "/"
-      }
-    })
-  ]
-});
+    "stackbitVersion": "~0.6.0",
+    "nodeVersion": "18",
+    "ssgName": "astro",
+    "contentSources": [],
+    "postInstallCommand": "npm i --no-save @stackbit/types"
+    
+})
