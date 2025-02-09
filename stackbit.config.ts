254f4c152aa88c5ec3bf35a3f01a2b1e6d518721
balamm <snmbala@gmail.com>
2025-02-09 23:21:52 +0530

    astro config stack

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,34 @@
 import { defineStackbitConfig } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
-import { allModels } from "./.stackbit/models"; // Ensure the correct path
 
 export default defineStackbitConfig({
-    stackbitVersion: "~0.7.0",
-    ssgName: "astro",
-    nodeVersion: "18",
+  stackbitVersion: "~0.6.0",
+  ssgName: "custom",
+  nodeVersion: "18",
+  devCommand: "node_modules/.bin/astro dev --port {PORT} --hostname 127.0.0.1",
+  experimental: {
+    ssg: {
+      name: "Astro",
+      logPatterns: {
+        up: ["is ready", "astro"],
+      },
+      directRoutes: {
+        "socket.io": "socket.io",
+      },
+      passthrough: ["/vite-hmr/**"],
+    },
+  },
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["src/content/blog"],
+      models: [],
+      assetsConfig: {
+        referenceType: "static",
+        staticDir: "src/content",
+        uploadDir: "_images",
+        publicPath: "/src/content/",
+      },
+    }),
+  ],
 });
